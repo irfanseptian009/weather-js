5aba2b36fedb38b73a3757f1ba7860adb1658731
irfan septian <ivanseftian79@gmai.com>
2024-05-30 22:31:34 +0700

    fatching data

--- script.js
+++ script.js
@@ -3,19 +3,7 @@ const searchButton = document.querySelector(".bx-search");
 
 const API_KEY = "ae02e446861bd64c7ae65f0dbaf5f215";
 
-const getWeatherDetails = async (countryName, lat, lon) => {
-  const WEATHER_API_URL = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=wind_speed_10m,wind_direction_10m&hourly=temperature_2m,relative_humidity_2m&daily=weather_code,temperature_2m_max,temperature_2m_min`;
-  //   const WEATHER_API_URL = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=temperature_2m_max,temperature_2m_min&timezone=Asia%2FBangkok`;
-
-  try {
-    const response = await fetch(WEATHER_API_URL);
-    const data = await response.json();
-    console.log(data); // Process or display the weather data here
-  } catch (error) {
-    alert("An error occurred fetching weather data forecast");
-  }
-};
-
+// get coordinate using openweather api
 const getCountryCoordinates = async () => {
   const countryName = countryInput.value.trim();
   if (!countryName) return;
@@ -33,4 +21,18 @@ const getCountryCoordinates = async () => {
   }
 };
 
+// get weatherDetail for open-meteor api
+const getWeatherDetails = async (countryName, lat, lon) => {
+  const WEATHER_API_URL = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=wind_speed_10m,wind_direction_10m&hourly=temperature_2m,relative_humidity_2m&daily=weather_code,temperature_2m_max,temperature_2m_min`;
+
+  try {
+    const response = await fetch(WEATHER_API_URL);
+    const data = await response.json();
+    console.log(data); // Process or display the weather data here
+  } catch (error) {
+    alert("An error occurred fetching weather data forecast");
+  }
+};
+
 searchButton.addEventListener("click", getCountryCoordinates);
+countryInput.addEventListener("keyup", (e) => e.key === "Enter" && getCityCoordinates());
