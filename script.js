b1e2e9b7ebec67eb95562b63eab1cba29183a1d4
irfan septian <ivanseftian79@gmai.com>
2024-06-04 02:17:51 +0700

    add pressure session

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const searchButton = document.querySelector(".search-btn");
 const currentWeatherDiv = document.querySelector(".current-weather");
 const sevenDaysCardsDiv = document.querySelector(".sevenDays-cards");
 const hourlyCardsDiv = document.querySelector(".hourly-cards");
-const uvIndexCardDiv = document.querySelector(".uv-card");
+const pressureCardDiv = document.querySelector(".pressure-card");
 const windCardDiv = document.querySelector(".wind-card");
 
 const API_KEY = "ae02e446861bd64c7ae65f0dbaf5f215"; // API key for OpenWeatherMap API
@@ -37,6 +37,8 @@ async function getWeatherDetails(cityName, latitude, longitude) {
     const dailyForecast = data.daily;
     const hourlyForecast = data.hourly;
 
+    console.log(data);
+
     // process for current weather
     currentWeatherDiv.innerHTML = `
 	<div class="current-weather">
@@ -44,40 +46,48 @@ async function getWeatherDetails(cityName, latitude, longitude) {
         <h2>${cityName}</h2>
 		<img 
 		class="weather-icon" 
-		src=${wmo[currentWeather.weather_code].day.image} alt="Weather Icon" />
-        <h6>Wind Speed: ${currentWeather.wind_speed_10m} m/s</h6>
-        <h6>Wind Direction: ${currentWeather.wind_direction_10m}°</h6>
+		<img src=${wmo[currentWeather.weather_code].day.image} alt="Weather-Icon"/>
+        <h4  class="temperature">${currentWeather.apparent_temperature}°C</h4>
+        <h5 class="description">${wmo[currentWeather.weather_code].day.description}</h5>
 		<div class="current-cards">
 		<div class="current-card">
-		  <h3>Humidity</h3>
+		<i class="bx bx-water"></i>
+		<h3> Humidity</h3>
 		  <ul>
 			<li>
-			  <h4>${currentWeather.relative_humidity_2m}</h4>
+			  <h4>${currentWeather.relative_humidity_2m}%</h4>
+			  <h6>the due point is 25°C</h6>
 			</li>
 		  </ul>
 		</div>
 		<div class="current-card">
-		  <h3>precipitation</h3>
+		<i class='bx bx-cloud-drizzle'></i>
+		  <h3> precipitation</h3>
 		  <ul>
 			<li>
-			  <h4>${currentWeather.precipitation}</h4>
+			  <h4>${currentWeather.precipitation} mm"</h4>
+			  <h6>in last 24h <br>excepted in next 24h</h6>
 			</li>
 		  </ul>
 		</div>
 		<div class="current-card">
+		<i class='bx bx-timer'></i> 
 		  <h3>Interval</h3>
 		  <ul>
 			<li>
 			  <h4>
-			  ${currentWeather.interval}</h4>
+			  ${currentWeather.interval}s</h4>
+			  <h6>an interval scale</h6>
 			</li>
 		  </ul>
 		</div>
 		<div class="current-card">
-		  <h3>apparent temperature</h3>
+		<i class='bx bx-cloud'></i>
+		  <h3> Cloud Cover</h3>
 		  <ul>
 			<li>
-			  <h4>${currentWeather.apparent_temperature}</h4>
+			  <h4>${currentWeather.cloud_cover}%</h4>
+			  <h6> fraction of the sky clouds on average</h6>
 			</li>
 		  </ul>
 		</div>
@@ -89,41 +99,60 @@ async function getWeatherDetails(cityName, latitude, longitude) {
     sevenDaysCardsDiv.innerHTML = "";
     for (let i = 0; i < dailyForecast.time.length; i++) {
       sevenDaysCardsDiv.innerHTML += `
+	  
     <li class="card">
-      <h3>${cityName}</h3>
+      <h3>${dailyForecast.time[i]}</h3>
 	  <img src=${wmo[dailyForecast.weather_code[i]].day.image} alt="Weather Icon" />
-      <h6>Max Temp: ${dailyForecast.temperature_2m_max[i]}</h6>
-      <h6>Min Temp: ${dailyForecast.temperature_2m_min[i]}</h6>
+      <h6 style="color: chartreuse; font-size: 20px; ">${
+        dailyForecast.temperature_2m_max[i]
+      }°C</h6>
+	  <h6>UV Index: ${dailyForecast.uv_index_max[i]}</h6>
     </li>`;
     }
 
     // process for hourly forecast
     hourlyCardsDiv.innerHTML = "";
-    for (let i = 0; i < Math.min(7, hourlyForecast.time.length); i++) {
+    for (let i = 0; i < Math.min(24, hourlyForecast.time.length); i++) {
       hourlyCardsDiv.innerHTML += `
     <li class="card">
+	<h3>${hourlyForecast.time[i]}</h3>
 	<img src=${wmo[hourlyForecast.weather_code[i]].day.image} alt="Weather Icon" />
-      <h6>Max Temp: ${dailyForecast.temperature_2m_max[i]}</h6>
       <h6>Temp: ${hourlyForecast.temperature_2m[i]}</h6>
       <h6>Humidity: ${hourlyForecast.relative_humidity_2m[i]}</h6>
     </li>`;
     }
 
     // process for UV index
-    uvIndexCardDiv.innerHTML = `
+    pressureCardDiv.innerHTML = `
+	<h2><i class="bx bxs-thermometer"></i>PRESSURE</h2>
+
+	<ul>
+	<img
+	src="assets/images/pressure-2.png"
+	alt="pressure"
+	style="height: 120px; width: 180px; opacity:30%;"
+  />
 		<li>
-		<h3>${currentWeather.pressure_msl}</h3>
-		<h3>${currentWeather.surface_pressure}</h3>
+		<h3>${currentWeather.pressure_msl} hPa</h3>
+		<h3>${currentWeather.surface_pressure} hPa</h3>
 		</li>
-		</ul>
+	</ul>
+		
 	`;
     //  process for wind directions
     windCardDiv.innerHTML = `
+	<h2><i class='bx bx-wind' ></i>WIND</h2>
+	<img
+	src="assets/images/compass-3.png"
+	alt="compass"
+	style="height: 120px; width: 120px"
+  	/>
 		<li>
-		<h3>${currentWeather.wind_direction_10m}</h3>
-		<h3>${currentWeather.wind_gusts_10m}</h3>
-		<h3>${currentWeather.wind_speed_10m}</h3>
+		<h3>${currentWeather.wind_direction_10m}°</h3>
+		<h3>${currentWeather.wind_gusts_10m}km/h</h3>
+		<h3>${currentWeather.wind_speed_10m}km/h</h3>
 		</li>
+	</ul>
 	`;
   } catch (error) {
     alert("An error occurred while fetching the weather details!");
